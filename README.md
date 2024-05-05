6edf472169c70e36e8db38d749e3bf89993a6537
Deyky Pinheiro <pinheiromikael96@gmail.com>
2024-05-05 14:52:04 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## How to run
+```
+npm i
+tsc app/app.ts
+node app/app.js
+```
+
+## Remove all .js
+rm -rf app/**/*.js
+rm -rf app/*.js
---
+++ app/models/PaperDataModel.ts
@@ -0,0 +1,12 @@
+export interface PaperDataModel {
+    id: string,
+    price: number,
+    time: number,
+    exchange: string,
+    quoteType: string,
+    marketHours: string,
+    changePercent: number,
+    dayVolume: number,
+    change: number,
+    priceHint: number
+}
